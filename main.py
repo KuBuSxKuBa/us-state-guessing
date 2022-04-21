30d3383058dbb4ee934d23695ce330c4634b2588
_sohatzky <68334609+KuBuSxKuBa@users.noreply.github.com>
2022-04-21 18:30:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,16 +25,24 @@ data_list = data["state"].to_list()
 is_on = True
 already_guessed = 0
 already_guessed_list = []
+all_states = []
+for i in data:
+    all_states.append(data["state"].to_string(index=False))
 while is_on:
     guess = s.textinput(title=f"{already_guessed}/50", prompt="What's another state's name?")
     guess = guess.title()
+    if guess == "exit":
+        left = [state for state in all_states if state not in already_guessed_list]
+        # left = []
+        # for i in all_states:
+        #     if i not in already_guessed_list:
+        #         left.append(i)
+        break
     if guess not in already_guessed_list:
         if guess in data_list:
             write(guess)
+            already_guessed_list.append(guess)
             already_guessed += 1
-            print("Score!")
-        else:
-            print("L bozo")
     else:
         pass
 # for i in data["state"]:
